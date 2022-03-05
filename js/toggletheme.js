9b5e7a62e7d476b4fe4ecda03845658b55ffe057
iokepasouza <iokepasouza@gmail.com>
2022-03-04 14:06:16 -1000

    content update

--- js/toggletheme.js
+++
@@ -1,9 +0,0 @@
-function ChangeTheme() {
-    var element = document.body
-
-    if (isChcked) {
-        body.classList.add('theme-white.css')
-    } else {
-        body.classList.remove('theme-blk.css')
-    }
-}
